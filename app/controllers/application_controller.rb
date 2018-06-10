66119d34523ee2c594612f535bf77c36e4b1639c
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-06-10 14:56:00 +0200

    Allow to pass first name and last name params during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_paramters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
