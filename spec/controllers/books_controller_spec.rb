16a3c16bbd8b95bc38a10d5dcc9596d6291bc7ed
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-06-20 15:41:12 +0200

    Add spec for current books controller behaviour

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  let(:user) { create(:user) }
+  let(:book1) { create(:book, title: "Harry Potter pt 1") }
+  let(:book2) { create(:book, title: "Harry Potter pt 2") }
+  let(:book3) { create(:book, title: "Harry Potter pt 3") }
+  let(:book4) { create(:book, title: "Something different") }
 
+  before do
+    sign_in(user)
+  end
+
+  describe 'GET #index' do
+    context 'when user search with a term' do
+      context 'and it does match one book' do
+        before do
+          get :index, params: { term: book4.title }
+        end
+
+        it 'returns only this one book' do
+          expect(assigns[:books]).to eq [ book4 ]
+        end
+      end
+
+      context 'and it does match multiple books' do
+        before do
+          get :index, params: { term: 'Potter' }
+        end
+
+        it 'returns all books that match the term' do
+          expect(assigns[:books]).to include(book1, book2, book3)
+        end
+
+      end
+
+      context 'and it does not match any books' do
+        before do
+          get :index, params: { term: 'Nothing' }
+        end
+
+        it 'return empty array' do
+          expect(assigns[:books]).to eq []
+        end
+      end
+    end
+
+    context 'when user does not search with a term' do
+      before do
+        get :index
+      end
+
+      it 'returns all books' do
+        expect(assigns[:books]).to include(book1, book2, book3, book4)
+      end
+    end
+  end
 end
+
+# expect(assigns[:results]).to eq results
