3f0a76fb8d42bf30b30770ad9d7fc3806bcdf62b
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-06-21 08:11:15 +0200

    Add author name scopes to author and book

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
 
   validates :name, presence: true
+
+  scope :with_name, lambda { |name| where("name ilike ?", "%#{name}%") }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :author_id, presence: true
+
+  scope :with_author, lambda { |name| joins(:author).merge(Author.with_name(name)) }
 end
