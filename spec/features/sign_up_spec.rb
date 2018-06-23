2ef1137846f7af7532e2fea2b17e00bc2912ac7d
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-06-23 09:26:57 +0200

    Add feature test for sign up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,14 +1,82 @@
 require 'rails_helper'
+require 'support/pages/sign_up_page'
 
 feature 'Sign up' do
+  let(:sign_up_page) { SignUpPage.new }
+
+  before do
+    sign_up_page.load
+  end
+
   scenario 'with valid data' do
-    visit '/users/sign_up'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'First name', with: 'John'
-    fill_in 'Last name', with: 'Doe'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_button 'Sign up'
+    sign_up_page.fill_in_form(
+      'user@example.com',
+      'John',
+      'Doe',
+      'password',
+      'password'
+    )
+    sign_up_page.submit_btn.click
     expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'without required fields' do
+    sign_up_page.fill_in_form(
+      '',
+      '',
+      '',
+      '',
+      ''
+    )
+    sign_up_page.submit_btn.click
+    within sign_up_page.error_explanation do
+      expect(page).to have_content 'Email can\'t be blank'
+      expect(page).to have_content 'First name can\'t be blank'
+      expect(page).to have_content 'Last name can\'t be blank'
+      expect(page).to have_content 'Password can\'t be blank'
+    end
+  end
+
+  scenario 'with invalid email' do
+    sign_up_page.fill_in_form(
+      'user.example.com',
+      'John',
+      'Doe',
+      'password',
+      'password'
+    )
+    sign_up_page.submit_btn.click
+    within sign_up_page.error_explanation do
+      expect(page).to have_content 'Email is invalid'
+    end
+  end
+
+  scenario 'with too short password' do
+    sign_up_page.fill_in_form(
+      'user@example.com',
+      'John',
+      'Doe',
+      'pass',
+      'pass'
+    )
+    sign_up_page.submit_btn.click
+    within sign_up_page.error_explanation do
+      expect(page).to have_content 'Password is too short'
+    end
+  end
+
+  scenario 'without password confirmation' do
+    sign_up_page.fill_in_form(
+      'user@example.com',
+      'John',
+      'Doe',
+      'password',
+      ''
+    )
+    sign_up_page.submit_btn.click
+    within sign_up_page.error_explanation do
+      expect(page)
+        .to have_content 'Password confirmation doesn\'t match Password'
+    end
   end
 end
---
+++ spec/support/pages/sign_up_page.rb
@@ -0,0 +1,23 @@
+class SignUpPage < SitePrism::Page
+  set_url '/users/sign_up'
+
+  element :error_explanation, '#error_explanation'
+
+  element :email, '#user_email'
+  element :first_name, '#user_first_name'
+  element :last_name, '#user_last_name'
+  element :password, '#user_password'
+  element :password_confirmation, '#user_password_confirmation'
+  element :submit_btn, '.actions > input[type="submit"]'
+  element :login_link, 'a[href="/users/sign_in"]'
+
+  def fill_in_form(email = nil, first_name = nil,
+                   last_name = nil, password = nil,
+                   password_confirmation = nil)
+    self.email.set(email)
+    self.first_name.set(first_name)
+    self.last_name.set(last_name)
+    self.password.set(password)
+    self.password_confirmation.set(password_confirmation)
+  end
+end
