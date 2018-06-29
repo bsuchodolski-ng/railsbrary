0b3d498ae0c6fad0ed4a6a63dd8c1372ba524908
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-06-29 08:43:24 +0200

    Validate uniquness of author's name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   scope :with_name, lambda { |name| where("name ilike ?", "%#{name}%") }
 end
