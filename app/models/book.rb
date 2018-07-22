7f8da5e913794212c865ae2e5f4dff03b27a1b5d
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-07-22 20:05:52 +0200

    Add published_at scopes to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,6 @@ class Book < ApplicationRecord
   validates :author_id, presence: true
 
   scope :with_author, lambda { |name| joins(:author).merge(Author.with_name(name)) }
+  scope :published_after, lambda { |start_date| where("published_at >= ?", start_date ) }
+  scope :published_before, lambda { |end_date| where("published_at <= ?", end_date ) }
 end
