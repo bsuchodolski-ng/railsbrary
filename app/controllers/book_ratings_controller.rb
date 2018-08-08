cf705511656ccacb76dd905899f63d45bd13ee02
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-08-08 21:04:27 +0200

    add update action to book ratings controller

--- app/controllers/book_ratings_controller.rb
+++ app/controllers/book_ratings_controller.rb
@@ -5,6 +5,18 @@ class BookRatingsController < ApplicationController
     @book_rating.save
   end
 
+  def update
+    @book_rating = BookRating.find_by(book_id: book_rating_params[:book_id],
+                                      user_id: book_rating_params[:user_id])
+    if book_rating_params[:rating].present?
+      @book_rating.update(book_rating_params)
+      @book_rating.save
+    else
+      @book_rating.destroy
+      @book_rating = @book_rating = BookRating.new(book_rating_params.except(:rating))
+    end
+  end
+
   private
 
   def book_rating_params
