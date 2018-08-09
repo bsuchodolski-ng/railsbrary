4d0a3709f3a732e5d195866f9730c5f97bf923e4
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-08-09 18:55:13 +0200

    add specs for book rating callback

--- spec/models/book_rating_spec.rb
+++ spec/models/book_rating_spec.rb
@@ -8,4 +8,33 @@ RSpec.describe BookRating, type: :model do
   # it { is_expected.to validate_uniqueness_of(:book_id).scoped_to(:user_id) }
   it { is_expected.to belong_to(:book) }
   it { is_expected.to belong_to(:user) }
+
+  describe '#recalculate_book_average_rating' do
+    let(:book_rating) { create(:book_rating) }
+    let(:book) { book_rating.book }
+
+    it 'triggers book to recalculate average rating' do
+      expect(book).to receive(:recalculate_average_rating)
+      book_rating.send(:recalculate_book_average_rating)
+    end
+  end
+
+  describe 'callbacks' do
+    let(:book_rating) { create(:book_rating) }
+    let(:new_book_rating) { build(:book_rating) }
+    it 'triggers #recalculate_book_average_rating on create' do
+      expect(new_book_rating).to receive(:recalculate_book_average_rating)
+      new_book_rating.save
+    end
+
+    it 'triggers #recalculate_book_average_rating on update' do
+      expect(book_rating).to receive(:recalculate_book_average_rating)
+      book_rating.save
+    end
+
+    it 'triggers #recalculate_book_average_rating on destroy' do
+      expect(book_rating).to receive(:recalculate_book_average_rating)
+      book_rating.destroy
+    end
+  end
 end
