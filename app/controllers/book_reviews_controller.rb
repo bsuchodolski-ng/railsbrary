7ec82164de25856ee9474a71f2648c2371c4e3ac
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-08-12 10:55:36 +0200

    handle book review create

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -2,4 +2,19 @@ class BookReviewsController < ApplicationController
   def new
     @book_review = BookReview.new(book_id: params[:book_id], user_id: current_user.id)
   end
+
+  def create
+    @book_review = BookReview.new(book_review_params)
+    if @book_review.save
+      render
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_review_params
+    params.require(:book_review).permit(:book_id, :user_id, :body)
+  end
 end
