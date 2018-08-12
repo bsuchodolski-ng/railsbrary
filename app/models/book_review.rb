d0efc66ae5d8b40d4ebbe6b1bdc423d2bf8cdac3
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-08-12 07:01:20 +0200

    add rating method to book review

--- app/models/book_review.rb
+++ app/models/book_review.rb
@@ -6,4 +6,14 @@ class BookReview < ApplicationRecord
                       uniqueness: { scope: :user_id, message: "was already reviewed" }
   validates :user_id, presence: true
   validates :body, presence: true
+
+  def rating
+    book_rating = book.book_ratings.find_by_user_id(user_id)
+    if book_rating
+      book_rating.rating
+    else
+      nil
+    end
+  end
+
 end
