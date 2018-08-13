6597d684205b1a3e100aa041c863af1159402ced
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-08-13 22:07:49 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+puts 'Create users and authors'
+20.times do
+  User.create(
+    email: FFaker::Internet.email,
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    password: 'password'
+  )
+
+  Author.create(name: FFaker::Name.name)
+end
+
+puts 'Create books'
+Author.all.each do |author|
+  5.times do
+    author.books.create(
+      title: FFaker::Book.title,
+      published_at: Time.at(rand((Time.now - (1500*60*24*365*5))..Time.now)),
+      cover_image: File.open(Rails.root + "app/assets/images/seeds/#{rand(1..5)}.png"),
+      description: FFaker::Lorem.paragraph(rand(3..8))
+    )
+  end
+end
+
+puts 'Create ratings and reviews'
+User.all.each do |user|
+  Book.all.each do |book|
+    book.book_ratings.create(user: user, rating: rand(1..5))
+    book.book_reviews.create(user: user, body: FFaker::Lorem.paragraph(rand(1..4)))
+  end
+end
+
+puts 'Create main user'
+User.create(
+  email: 'qwe@qwe.qwe',
+  first_name: 'John',
+  last_name: 'Doe',
+  password: 'password'
+)
